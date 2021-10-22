2db44c14023e33eeb16b56306f986ff5b4741ba0
Akash Thapa <magarthapa32@gmail.com>
2021-10-22 18:07:08 -0500

    data tansfering object for user table

--- src/main/java/com/ticket/manger/dto/UserSubmitionDto.java
+++ src/main/java/com/ticket/manger/dto/UserSubmitionDto.java
@@ -1,4 +1,65 @@
 package com.ticket.manger.dto;
 
+import com.ticket.manger.enums.RoleType;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+//Data transfering object to transfer data to database
 public class UserSubmitionDto {
+    private Long id;
+
+    @NotBlank
+    @NotNull
+    @Email
+    private String username;
+
+    @NotBlank
+    @NotNull
+    private String name;
+    private List<RoleType> roles;
+
+    public UserSubmitionDto() {
+    }
+
+    public UserSubmitionDto(Long id, @NotBlank @NotNull @Email String username, @NotBlank @NotNull String name, List<RoleType> roles) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<RoleType> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RoleType> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/com/ticket/manger/dto/UserWrapperDto.java
+++ src/main/java/com/ticket/manger/dto/UserWrapperDto.java
@@ -1,4 +1,25 @@
 package com.ticket.manger.dto;
 
+
+import java.util.List;
+
+//to get the all the userlist
 public class UserWrapperDto {
+
+    private List<UserDto> data ;
+
+    public UserWrapperDto() {
+    }
+
+    public UserWrapperDto(List<UserDto> data) {
+        this.data = data;
+    }
+
+    public List<UserDto> getData() {
+        return data;
+    }
+
+    public void setData(List<UserDto> data) {
+        this.data = data;
+    }
 }
