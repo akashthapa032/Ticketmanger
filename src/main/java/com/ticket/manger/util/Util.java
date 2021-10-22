3841c7d960389dec3bbf7879d3a941c7e7d74c3d
Akash Thapa <magarthapa32@gmail.com>
2021-10-22 17:34:11 -0500

    just create Util class component for adding date in expired on and to get unique values function

--- src/main/java/com/ticket/manger/util/Util.java
+++ src/main/java/com/ticket/manger/util/Util.java
@@ -2,8 +2,21 @@ package com.ticket.manger.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
 @Component
 public class Util {
+    public String getUniqueCode(){
+        UUID uuid = UUID.randomUUID(); //unique
+        return uuid.toString() + new Date().getTime(); //unique
+    }
 
+    public Date addDays (int day){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH,day);
+        return calendar.getTime();
+    }
 
 }
